23e87bcb4473a43e39ca9261a049728c485f61c4
aliyev-vladislav <dalv.veyila@gmail.com>
2024-05-10 00:35:07 +0300

    add example of working with packages in golang

---
+++ core/go.mod
@@ -0,0 +1,3 @@
+module github.com/aliyev-vladislav/learning-go/core
+
+go 1.22.2
---
+++ core/modules_pachages_imports/package_example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	format "github.com/aliyev-vladislav/learning-go/core/modules_pachages_imports/package_example/do-format"
+	"github.com/aliyev-vladislav/learning-go/core/modules_pachages_imports/package_example/math"
+)
+
+func main() {
+	num := math.Double(2)
+	output := format.Number(num)
+	fmt.Println(output)
+}
