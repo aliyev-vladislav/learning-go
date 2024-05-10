f0d8379c00a96b3930723cd2813a672d6c74e6db
aliyev-vladislav <dalv.veyila@gmail.com>
2024-05-10 18:02:29 +0300

    rename path

--- core/modules_packages_imports/package_example/main.go
+++ core/modules_packages_imports/package_example/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	format "github.com/aliyev-vladislav/learning-go/core/modules_pachages_imports/package_example/do-format"
-	"github.com/aliyev-vladislav/learning-go/core/modules_pachages_imports/package_example/math"
+	format "github.com/aliyev-vladislav/learning-go/core/modules_packages_imports/package_example/do-format"
+	"github.com/aliyev-vladislav/learning-go/core/modules_packages_imports/package_example/math"
 )
 
 func main() {
