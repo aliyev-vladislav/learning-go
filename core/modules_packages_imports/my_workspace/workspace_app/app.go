da25d2721fdf22d7635a9f8cb5003117ae7f645f
aliyev-vladislav <dalv.veyila@gmail.com>
2024-05-11 22:29:42 +0300

    using workspacs to modify modules simyltaneously

---
+++ core/modules_packages_imports/my_workspace/workspace_app/app.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/aliyev-vladislav/learning-go/core/modules_packages_imports/my_workspace/workspace_lib"
+)
+
+func main() {
+	fmt.Println(workspace_lib.AddNums(2, 3))
+}
---
+++ core/modules_packages_imports/my_workspace/workspace_lib/go.mod
@@ -0,0 +1,3 @@
+module github.com/aliyev-vladislav/learning-go/core/modules_packages_imports/my_workspace/workspace_lib
+
+go 1.22.3
